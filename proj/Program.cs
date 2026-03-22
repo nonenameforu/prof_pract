ca62f94858b2d79bc7f946bfb552033f32165707
__noname__ <139173318+nonenameforu@users.noreply.github.com>
2026-03-22 18:07:49 +0300

    add program class

--- proj/Program.cs
+++ proj/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using StockExchange.Market;
+using StockExchange.Crypto;
+using StockExchange.UI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var fiatService = new ExchangeRateService();
+        var cryptoService = new CryptoMarketService();
+        var dashboard = new ConsoleDashboard(fiatService, cryptoService);
+        var converter = new CurrencyConverter(fiatService);
+        var chart = new ChartPrinter();
+
+        Console.WriteLine("Запуск биржи... симулируем 10 тиков.\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            fiatService.SimulateTick();
+            cryptoService.SimulateTick();
+            Thread.Sleep(200);
+        }
+
+        dashboard.Render();
+
+        chart.Print("BTC", cryptoService.GetHistory("BTC"));
+        chart.Print("RUB", fiatService.GetHistory("RUB"));
+
+        converter.RunInteractive();
+    }
+}
--- proj/obj/Debug/net10.0/proj.AssemblyInfo.cs
+++ proj/obj/Debug/net10.0/proj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89834dc9ef6d90ea594620b08f9d3c2e0a394740")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ff79b369ecbae4051d5ffbbf1ef20926e1f768")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
